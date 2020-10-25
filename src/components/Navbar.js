64c8150adc947babb986aa721cecdf061c0b2684
Kevin Morris <kevr@0cost.org>
2020-10-24 22:07:34 -0700

    Add components/Navbar.js
    Using Material UI's navigation bar style, Navbar.js is accompanied
    by Navbar.test.js to cover it's implementation.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
+
+const Navbar = ({ title }) => (
+  <AppBar position="static">
+    <Toolbar className="d-flex">
+      <div className="flex-fill text-left">
+        <a className="text-undecorated"
+          href="/"
+          alt="Homepage"
+        >
+          <Typography variant="h6">
+            {title}
+          </Typography>
+        </a>
+      </div>
+
+      <Button color="inherit">
+        Projects
+      </Button>
+    </Toolbar>
+  </AppBar>
+);
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default Navbar;
