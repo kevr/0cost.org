1191e4a12f79c8f9a5f7f8f37344defb1f24b07e
Kevin Morris <kevr@0cost.org>
2020-10-25 21:37:15 -0700

    App.js: updated to use <Navbar>
    Our main application Navbar.
    
    Brand Route: / - <Navbar.title>
    
    Extra Routes:
        /projects - Projects
        /reference - Reference
        /about - About (activates submenu)
            /about/org - Organization
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/App.js
+++ src/App.js
@@ -9,11 +9,28 @@ import Navbar from './components/Navbar';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const routes = [
+    // Direct nav items to routes.
+    { title: "Projects", path: "/projects" },
+    { title: "Reference", path: "/reference" },
+
+    // About is a nav item with a submenu to subitems.
+    {
+      // An id ending with "-nav-menu" styles it to match the navbar.
+      id: "about-nav-menu",
+      title: "About",
+      path: "/about",
+      children: [
+        { title: "Organization", path: "/about/org" }
+      ]
+    }
+  ];
+
   return (
     <Router>
       <div className="App">
-        <Navbar title="ZeroCost" />
+        <Navbar title="ZeroCost" routes={routes} />
 
         <Switch>
           <Route path="/">
