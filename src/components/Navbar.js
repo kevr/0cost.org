5322f85a5e7083dde80f030d5f7e1b1099d2ea1b
Kevin Morris <kevr@0cost.org>
2020-10-25 21:32:02 -0700

    Use NavItems in Navbar + active navbar-brand support
    NavItems come with a built-in active class toggle, but
    Navbar does not; this patch includes a small className
    constructions inside of Navbar depending on the current
    location path being / or not.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
+import NavItem from './NavItem';
 
-const Navbar = ({ title }) => (
-  <AppBar position="static">
-    <Toolbar className="d-flex">
-      <div className="flex-fill text-left">
-        <Link className="text-undecorated" to="/">
-          <Typography variant="h6">
-            {title}
-          </Typography>
-        </Link>
-      </div>
+/**
+ * Main application navigation bar component.
+ *
+ * For documentation on Navbar's properties, see Navbar.propTypes
+ * at the bottom of this module.
+ **/
+const Navbar = ({ title, routes, location }) => {
+  // navbar-brand className.
+  let className = "text-undecorated text-white text-normal";
+  if(location.pathname === "/")
+    className = className.concat(" active");
 
-      <Button color="inherit">
-        Projects
-      </Button>
-      <Button color="inherit">
-        References
-      </Button>
-      <Button color="inherit">
-        About
-      </Button>
-    </Toolbar>
-  </AppBar>
-);
+  return (
+    <AppBar position="static">
+      <Toolbar className="d-flex">
+        <div className="flex-fill text-left">
+          <Link className="text-undecorated navbar-brand" to="/">
+            <Button color="inherit" className={className}>
+              <Typography variant="h6">
+                {title}
+              </Typography>
+            </Button>
+          </Link>
+        </div>
 
-Navbar.propTypes = {
-  title: PropTypes.string.isRequired
+        {routes.map((route, i) => (
+          <NavItem key={i}
+            id={route.id}
+            title={route.title}
+            children={route.children}
+            path={route.path}
+          />
+        ))}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  // The Navbar's branding.
+  title: PropTypes.string.isRequired,
+
+  // The Navbar's routes (default: []).
+  routes: PropTypes.array.isRequired,
+
+  // React-Router location provided by withRouter. This property
+  // should **NOT** be overridden by Navbar's user.
+  location: PropTypes.object.isRequired
+};
+
+Navbar.defaultProps = {
+  routes: []
+};
+
+export default withRouter(Navbar);
