8ed6a0df889a1812f1c74a9565a343c073ad8ea2
Kevin Morris <kevr@0cost.org>
2020-10-26 22:14:39 -0700

    App.js: Remove unused module imports.
    Thankfully, this caused `npm run build` to fail
    due to warnings related to these imports.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Page from './pages';
 import './App.css';
